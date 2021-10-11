78a6242393b32d2487c4a0b8720d42091ecca3f8
FairaIchsa <zelov02@yandex.ru>
2021-10-12 02:13:12 +0300

    updated save for Order Model

--- mainapp/models.py
+++ mainapp/models.py
@@ -35,41 +35,84 @@ class Order(models.Model):
         ('cashless', 'Безналичный расчет'),
         ('credit', 'Кредит'),
         ('barter', 'Бартер'),
+        ('sell', 'Взаимозачет')
     )
 
     customer = models.ForeignKey(Customer, verbose_name='Покупатель', on_delete=models.CASCADE,
                                  related_name='orders')
     needed_product = models.ForeignKey(Product, verbose_name='Запаришваемый продукт', related_name='first_product',
-                                       on_delete=models.CASCADE)
-    needed_quantity = models.PositiveIntegerField(verbose_name='Количество нужного товара')
-    needed_cost = models.DecimalField(default=0, max_digits=7, decimal_places=2, editable=False,
-                                      verbose_name='Стоимость нужного товара')
-    pay_way = models.CharField(max_length=255, choices=pay_way_choices)
+                                       on_delete=models.CASCADE, blank=True, null=True)
+    needed_quantity = models.PositiveIntegerField(verbose_name='Количество нужного товара', blank=True, null=True)
+    needed_cost = models.DecimalField(default=0, max_digits=7, decimal_places=2,
+                                      verbose_name='Стоимость нужного товара', blank=True, null=True)
+    pay_way = models.CharField(max_length=255, choices=pay_way_choices, verbose_name='способ оплаты')
     given_product = models.ForeignKey(Product, verbose_name='Отдаваемый продукт', related_name='second_product',
                                       on_delete=models.CASCADE, blank=True, null=True)
-    given_quantity = models.PositiveIntegerField(verbose_name='Количество отдаваемого продукта', editable=False,
+    given_quantity = models.PositiveIntegerField(verbose_name='Количество отдаваемого продукта',
                                                  blank=True, null=True)
     given_cost = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='Стоимость отдаваемого продукта',
-                                     editable=False, blank=True, null=True)
+                                     blank=True, null=True)
 
     def __str__(self):
         return f"{self.id} {self.customer}: {self.needed_product.name} ({self.needed_quantity} шт.)"
 
     def save(self, *args, **kwargs):
-        self.needed_cost = self.needed_product.price * self.needed_quantity
-        self.needed_product.quantity -= self.needed_quantity
-        self.needed_product.save()
-        if self.pay_way == 'cashless':
-            self.customer.current_count -= self.needed_cost
-            self.customer.all_count += self.needed_cost
-        if self.pay_way == 'credit':
-            self.customer.current_credit += self.needed_cost
-            #   в данном случае сумма покупок не обновляется
-        if self.pay_way == 'barter':
-            self.given_quantity = math.ceil(self.needed_cost / self.given_product.price)
-            self.given_cost = self.given_product.price * self.given_quantity
+        # if self.pay_way == 'cash':
+        #     self.needed_product.quantity -= self.needed_quantity
+        #     self.needed_cost = self.needed_product.price * self.needed_quantity
+        #     self.customer.all_count += self.needed_cost
+        #     self.customer.save()
+        #     self.needed_product.save()
+        # elif self.pay_way == 'cashless':
+        #     self.needed_product.quantity -= self.needed_quantity
+        #     self.needed_cost = self.needed_product.price * self.needed_quantity
+        #     if self.needed_cost <= self.customer.current_count:
+        #         self.customer.current_count -= self.needed_cost
+        #     else:   #   если денег на счету недостаточно
+        #         self.pay_way = 'credit'
+        #         self.customer.credit_count += self.needed_cost
+        #     self.customer.all_count += self.needed_cost
+        #     self.customer.save()
+        #     self.needed_product.save()
+        # elif self.pay_way == 'barter':
+        #     self.needed_product.quantity -= self.needed_quantity
+        #     self.needed_cost = self.needed_product.price * self.needed_quantity
+        #     self.given_quantity = math.ceil(self.needed_cost / self.given_product.price)
+        #     self.given_product.quantity += self.given_quantity
+        #     self.given_cost = self.given_product.price * self.given_quantity
+        #     self.needed_product.save()
+        #     self.given_product.save()
+        # elif self.pay_way == 'sell':
+        #     self.given_cost = self.given_product.price * self.given_quantity
+        #     self.given_product.quantity += self.given_quantity
+        #     self.customer.current_credit -= self.given_cost
+        #     self.customer.save()
+        #     self.given_product.save()
+
+        if (self.pay_way == 'cash') or (self.pay_way == 'cashless') or (self.pay_way == 'barter'):
+            self.needed_product.quantity -= self.needed_quantity
+            self.needed_cost = self.needed_product.price * self.needed_quantity
+            if (self.pay_way == 'cash') or (self.pay_way == 'cashless'):
+                self.customer.all_count += self.needed_cost
+                if self.pay_way == 'cashless':
+                    if self.needed_cost <= self.customer.current_count:
+                        self.customer.current_count -= self.needed_cost
+                    else:   # если денег на счету недостаточно (мб стоит обнулить текущий счет и снизить )
+                        self.pay_way = 'credit'
+                        self.customer.credit_count += self.needed_cost - self.customer.current_count
+                        self.customer.current_count = 0
+        if (self.pay_way == 'barter') or (self.pay_way == 'sell'):
+            if self.pay_way == 'barter':
+                self.given_quantity = math.ceil(self.needed_cost / self.given_product.price)
             self.given_product.quantity += self.given_quantity
-            self.given_product.save()
-            self.customer.current_credit += self.given_cost
+            if self.pay_way == 'sell':
+                self.given_cost = self.given_product.price * self.given_quantity
+                if self.given_cost < self.customer.current_credit:
+                    self.customer.current_credit -= self.given_cost
+                else:
+                    self.customer.current_count += self.given_cost - self.customer.current_credit
+                    self.customer.current_credit = 0
+        self.needed_product.save()
+        self.given_product.save()
         self.customer.save()
         super().save(*args, **kwargs)
